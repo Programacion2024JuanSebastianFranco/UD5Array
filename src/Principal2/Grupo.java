af142606e0175a12f706aaa8abafe761791f7786
Franco <juafracas@alu.edu.gva.es>
2024-12-11 17:24:29 +0100

    Leer Alumnos

--- src/Principal2/Grupo.java
+++ src/Principal2/Grupo.java
@@ -2,19 +2,38 @@ package Principal2;
 
 import Principal1.Asignatura;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Grupo {
     private String nombreGrupo;
     private String[] alumnos;
     private Asignatura[] asignaturas;
     private int totalAlumnos;
     private int totalAsignaturas;
-
+    private Scanner scan = new Scanner(System.in);
 
 
     public Grupo(String nombreGrupo, int totalAlumnos, int totalAsignaturas) {
         this.nombreGrupo = "DAM 1K";
-        this.alumnos = new String[24];
-        this.asignaturas = new Asignatura[5];
+        this.alumnos = new String[totalAlumnos];
+        this.asignaturas = new Asignatura[totalAsignaturas];
+        this.totalAlumnos = totalAlumnos;
+        this.totalAsignaturas = totalAsignaturas;
+    }
+
+    public void leerAlumnos(){
+        if (alumnos != null) {
+            for (int i = 0; i < totalAlumnos; i++) {
+                System.out.println("Nombre del alumno " + (i + 1) + ": ");
+                String alumno = scan.nextLine();
+                alumnos[i] = alumno;
+            }
+            Arrays.sort(alumnos);
+        }
+        else {
+            System.out.println("Sin alumnos asignados");
+        }
     }
 
 
--- src/Principal2/Principal2.java
+++ src/Principal2/Principal2.java
@@ -1,4 +1,13 @@
 package Principal2;
 
 public class Principal2 {
+    public static void main(String[] args) {
+
+        Principal2 principal2 = new Principal2();
+
+        Grupo grupo = new Grupo("1KDAM", 2, 2);
+
+        grupo.leerAlumnos();
+
+    }
 }
